359410b0787896e840f81d326d3317ca5aba39ce
shahab_27 <shahabfardeen2019@gmail.com>
2025-11-12 14:25:53 +0530

    add createServer.js to initialize HTTP server

---
+++ server/createServer.js
@@ -0,0 +1,10 @@
+import http from 'http'
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type' :'text/plain '})
+    res.end('Hello , Welcome to my Node js server!')
+})
+
+// server listens on port 3000
+server.listening(3000,()=>{
+    console.log("Server is runnung on 3000 port")
+})
